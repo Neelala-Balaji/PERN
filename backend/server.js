785c86740bef317c8a17be210c26bb637782f73c
balaji-neelala <balaji.neelala.com>
2023-09-05 14:34:36 +0530

    added block list IP addresses

--- backend/server.js
+++ backend/server.js
@@ -25,10 +25,10 @@ pool.connect((err, client, done) => {
 });
 
 // Allow requests only from a specific origin
-const allowedOrigins = ["http://localhost:3000/"];
+const allowedOrigins = ["http://localhost:3000", "http://localhost:3001"];
 
 const corsOptions = {
-  origin: (origin, callback) => {
+  origin: function (origin, callback) {
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
